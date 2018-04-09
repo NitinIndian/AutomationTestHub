c79ce4ecec497d84ffad4de65f369dab0d178ae5
Nitin <Nitin@192.168.2.4>
2018-04-09 21:59:43 +0530

    Added Default Constructor

---
+++ JavaPractice/src/OopsPrograms/DefaultConstuctor.java
@@ -0,0 +1,16 @@
+package OopsPrograms;
+
+public class DefaultConstuctor {
+
+	public DefaultConstuctor() {
+		System.out.println("Default Constructor");
+	}
+
+	public static void main(String args[]){
+
+		new DefaultConstuctor();
+
+
+	}
+
+}
